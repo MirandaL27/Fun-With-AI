e1dd522cc7291b99a753656eefdcace1e97312b7
Cadenza <mirandalucsb@gmail.com>
2022-05-21 19:17:48 -0700

    styled responses. added local storage for responses

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -25,6 +25,7 @@ async function fetchData(promptText) {
       if (!respdata.error) {
           //call to display data here!
             responses.push({prompt: respdata.prompt, response: respdata.response});
+            saveResponses();
             displayResponses();
       } else {
         alert(respdata.error);
@@ -38,22 +39,26 @@ function displayResponses(){
 
     //create new responses
     containerEl=document.createElement("section");
-    containerEl.className ="response-container";
-    console.log(responses);
+    containerEl.className ="response-container black-background p-large m-large";
     for(let i=responses.length-1;i>=0;i--){
         let divEl = document.createElement("div");
+        divEl.className="dark-green-background p-small m-small rounded-corners light-green-border";
 
         let promptDiv = document.createElement("div");
+        promptDiv.className="p-small";
         let promptLableEl = document.createElement("h3");
         promptLableEl.textContent = "Prompt: ";
+        promptLableEl.className="p-small-bottom";
         let promptTextEl = document.createElement("p");
         promptTextEl.textContent = responses[i].prompt;
         promptDiv.appendChild(promptLableEl);
         promptDiv.appendChild(promptTextEl);
 
         let responseDiv = document.createElement("div");
+        responseDiv.className="p-small";
         let responseLabelEl = document.createElement("h3");
         responseLabelEl.textContent = "Response: ";
+        responseLabelEl.className="p-small-bottom";
         let responseTextEl = document.createElement("p");
         responseTextEl.textContent = responses[i].response;
         responseDiv.appendChild(responseLabelEl);
@@ -66,3 +71,23 @@ function displayResponses(){
 
     bodyEl.appendChild(containerEl);
 }
+
+function saveResponses(){
+    localStorage.setItem("responses",JSON.stringify(responses));
+}
+
+function clearResponses(){
+    localStorage.removeItem("responses");
+    responses.length = 0;
+    displayResponses();
+}
+
+function loadResponses(){
+    var storage = localStorage.getItem("responses");
+    if(storage){
+        responses = JSON.parse(storage);
+    }
+    displayResponses();
+}
+
+loadResponses();
